4acee2c88f56f5230f09fb588b350152f1216e31
Charles Huffman <huffmancharles14@gmail.com>
2024-07-09 17:53:55 -0500

    add some dummy books to array

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+const myLibrary = [
+    {
+        title: "The War of Art",
+        author: "Steven Pressfield",
+        pages: "192",
+        read: "no"
+    },
+    {
+        title: "The Little Book of Hygge",
+        author: "Meik Wiking",
+        pages: "240",
+        read: "no"
+    },
+    {
+        title: "Between Two Fires",
+        author: "Christopher Buehlman",
+        pages: "436",
+        read: "no"
+    },
+    {
+        title: "Atomic Habits",
+        author: "James Clear",
+        pages: "320",
+        read: "no"
+    }
+
+];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -5,5 +33,7 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function addBookToLibrary() {
 
+}
 
