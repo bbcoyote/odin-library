11fc9437ec332a058f8af467d4b7f22e6a4d7621
Charles Huffman <huffmancharles14@gmail.com>
2024-07-09 20:23:54 -0500

    add some css, get myLibrary to show on the dom

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const booksContainer = document.querySelector('.books-container');
+const addBtn = document.querySelector('.add-book');
+
 const myLibrary = [
     {
         title: "The War of Art",
@@ -33,7 +36,40 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+// Okay this will be a button on a form that calls a new Book() and each input will
+// be this. value. we could either make it a modal or a dialog or just display the form
+// on the page. we could make it the footer with a sticky position so it's always visible.
+
 function addBookToLibrary() {
 
 }
 
+// I want the books to be displayed in the center of the screen
+// In a ladder shapped fasion kind of like amazon
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const bookCard = document.createElement("div");
+        const title = document.createElement("span");
+        const author = document.createElement('span');
+        const pages = document.createElement('span');
+        const read = document.createElement('span');
+        const bookTitle = document.createElement("span");
+        const bookAuthor = document.createElement('span');
+        const bookPages = document.createElement('span');
+        const bookRead = document.createElement('span');
+        read.textContent = `Have you read?`;
+        pages.textContent = `Page Count`
+        author.textContent = `Author's Name`;
+        title.textContent = `Title`;
+        bookRead.textContent = `${myLibrary[i].read}`;
+        bookPages.textContent = `${myLibrary[i].pages}`
+        bookAuthor.textContent = `${myLibrary[i].author}`;
+        bookTitle.textContent = `${myLibrary[i].title}`;
+        booksContainer.appendChild(bookCard)
+        bookCard.append(title, bookTitle, author, bookAuthor, pages, bookPages, read, bookRead);
+    }
+}
+
+displayBooks()
+
+addBtn.addEventListener('click',)
